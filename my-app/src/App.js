7a90f126c114dd677c10c56d1c497e41ce4f1bf0
tokkaiiii <166105372+tokkaiiii@users.noreply.github.com>
2025-02-10 22:07:22 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd3 test
+          Learn GithubAction cicd1 test
         </a>
       </header>
     </div>
